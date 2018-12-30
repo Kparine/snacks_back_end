e5ebafeba6ce7d978b464658271084df0848d6db
Kevin Parine <kparine314@gmail.com>
2018-12-30 12:56:34 -0800

    ADD SNACK CONTROL

---
+++ src/controllers/snacks.js
@@ -0,0 +1,21 @@
+const model = require('../models/snacks')
+
+function getAll(req, res, next){
+  model.getAll()
+  .then(data => {
+    res.send(data)
+  })
+  .catch(next)
+}
+
+function getOne(req, res, next) {
+  model.getOne(parseInt(req.params.sId))
+  .then(data => {
+    if(!data) throw ({status: 404, message: 'Snack Not Found'})
+    return res.send({data})
+  })
+  .catch(next)
+}
+
+
+module.exports = { getAll, getOne}
--- src/models/reviews.js
+++ src/models/reviews.js
@@ -11,30 +11,45 @@ function getAll(id) {
 
 /////////////// GET ONE REVIEW ////////////////
 
-function getOne(id, rId) {
+function getOne(sId, rId) {
   return db('reviews')
-  .where({ snack_id: id, id: rId })
+  .where({ snack_id: sId, id: rId })
   .first()
 }
 /////////////// CREATE REVIEW ////////////////
 
-function create(){
-
+function create(sId, {title, text, rating, user_id}){
+  return db('reviews')
+  .insert({title, text, rating, snack_id: sId, user_id})
+  .returning('*')
+  .then(function([data]){
+    return data
+  })
 }
 
 /////////////// UPDATE REVIEW ////////////////
 
-function update(){
-
+function update(sId, rId, {title, text, rating}){
+  return db('reviews')
+  .update({title, text, rating})
+  .where({snack_id: sId, id: rId})
+  .returning('*')
+  .then(([data]) => {
+    return data
+  })
 }
 
 /////////////// REMOVE REVIEW ////////////////
 
-function remove(){
+function remove(sId, rId){
   return db('reviews')
   .del()
-  .where('id', id)
+  .where({snack_id: sId, id: rId })
   .returning('*')
+  .then(([data])=> {
+    delete data.id
+    return data
+  })
 }
 
 
